77a1aab78a984e98446d5690d0fcb45615fdaf24
Midoriin <fbyigit07@hotmail.com>
2020-02-04 17:49:53 +0300

    Rewrite

--- main.py
+++ main.py
@@ -2,105 +2,143 @@ import random
 import pyperclip
 
 done = False
-true_emote = ":green_heart:"
-false_emote = ":bomb:"
+
+bomb_emote = ":bomb:"
+number_emotes = (
+    '0️⃣',
+    '1️⃣',
+    '2️⃣',
+    '3️⃣',
+    '4️⃣',
+    '5️⃣',
+    '6️⃣',
+    '7️⃣',
+    '8️⃣',
+    '9️⃣',
+)
 
 
 def get_size():
     try:
-        size = int(input("Please input the length of the field (You shouldn't go over 12): "))
-        if size >= 1:
-            return size
-        else:
-            print("You must input a positive integer!")
-            get_size()
-
+        _size = int(input("Please input the length of the field (You shouldn't go over 12): "))
     except ValueError:
         print("You dumb? Try to input a proper number next time. I'm asking again...")
-        get_size()
+        return get_size()
+
+    if _size >= 1:
+        return _size
+    else:
+        print("Size can't be less than 1!")
+        return get_size()
 
 
 def get_difficulty():
     try:
-        difficulty = int(input("Please input the difficulty (0-100) (35-40 is ideal): "))
-        if 0 <= difficulty <= 100:
-            return difficulty
-        else:
-            print("Try to stick to the 0-100 rule. You must input a number between 0 and 100.")
-            get_difficulty()
-
+        _difficulty = int(input("Please input the difficulty (0-100) (25-30 is ideal): "))
     except ValueError:
         print("You dumb? Try to input a proper number next time. I'm asking again...")
-        get_difficulty()
+        return get_difficulty()
+
+    if 0 <= _difficulty <= 100:
+        return _difficulty
+    else:
+        print("Try to stick to the 0-100 rule. You must input a number between 0 and 100.")
+        return get_difficulty()
 
 
 def status_check():
     status = input("Type 1 to continue creating, or 0 to leave the program: ")
+
     if status == "1":
         return
+
     elif status == "0":
         global done
         done = True
         return
+
     else:
         print("You dumb? I'm asking again...")
-        status_check()
+        return status_check()
 
 
-def copy_to_clipboard(text):
-    if len(text) > 2000:
+def copy_to_clipboard(_text: str):
+    if len(_text) > 2000:
         print("The character length exceeds 2000 characters! You'll most likely not able to post this on Discord."
               "However, I'm still going to copy it to your clipboard.")
     try:
-        pyperclip.copy(text)
+        pyperclip.copy(_text)
         print("It's been copied to your clipboard!")
+
     except Exception as e:
-        print("An error occured: \n", e)
+        print("An error occured while trying to copy to your clipboard: \n", e)
 
 
 def convert_to_text(_map):
-    text = ""
+    _text = ""
+
     for x in range(len(_map)):
         for y in range(len(_map)):
             if _map[x][y] == 1:
-                text += "||" + true_emote + "||"
+                _text += "||" + number_emotes[get_adjacent_bomb_count(_map, x, y)] + "||"
             else:
-                text += "||" + false_emote + "||"
-        text += "\n"
+                _text += "||" + bomb_emote + "||"
+        _text += "\n"
 
-    return text
+    return _text
 
 
-def create(size, difficulty):
+def get_adjacent_bomb_count(_map, x, y):
+    adjacent_coords = (
+        (x - 1, y - 1),
+        (x - 1, y),
+        (x - 1, y + 1),
+
+        (x, y - 1),
+        (x, y + 1),
+
+        (x + 1, y - 1),
+        (x + 1, y),
+        (x + 1, y + 1),
+    )
+
+    adjacent_bombs = []
+    for coord in adjacent_coords:
+        try:
+            adjacent_bombs.append(_map[coord[0]][coord[1]])
+        except IndexError:  # if outside of the map, pass
+            pass
+
+    adjacent_bombs = list(filter(lambda e: e == 0, adjacent_bombs))
+    return len(adjacent_bombs)
+
+
+def create(_size: int, _difficulty: int):
     _map = []
 
-    try:
-        for x in range(size):
-            _map.append([])
-            for y in range(size):
-                _random = random.randrange(101)
-                # if difficulty is high, chances of getting a higher number is less
-                if difficulty > _random:
-                    _map[x].append(0)
-                else:
-                    _map[x].append(1)
-
-    except Exception as e:
-        print(f"An error occurred while creating: \n", e)
+    for x in range(_size):
+        _map.append([])
+        for y in range(_size):
+            _random = random.randrange(101)
+            # if difficulty is high, chances of getting a higher number is less
+            if _difficulty > _random:  # if bomb
+                _map[x].append(0)
+            else:
+                _map[x].append(1)
 
     return _map
 
 
-while not done:
-    size = get_size()
+if __name__ == '__main__':
+    while not done:
+        size = get_size()
 
-    difficulty = get_difficulty()
+        difficulty = get_difficulty()
 
-    _map = create(size, difficulty)
+        created_map = create(size, difficulty)
 
-    text = convert_to_text(_map)
+        text = convert_to_text(created_map)
 
-    copy_to_clipboard(text)
+        copy_to_clipboard(text)
 
-    status_check()
-
+        status_check()
